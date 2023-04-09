c78c74065321245fe72219cdf9923318d29f92a8
Izzatrahimi <izzatrahimi27@gmail.com>
2023-04-09 21:51:59 +0800

    Initial Commit

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -3,6 +3,7 @@ package samples.demo;
 public class Location {
 	
 	private String location;
+	private String name;
 	
 	public void setLocation(String location) {
 		this.location = location;
@@ -10,6 +11,14 @@ public class Location {
 	
 	public String getLocation() {
 		return this.location;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
 	}
 
 	public static void main(String[] args) {
