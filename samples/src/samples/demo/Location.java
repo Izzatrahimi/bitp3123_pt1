161d62cf015521ac4b32a558775194244d0b91f9
Izzatrahimi <izzatrahimi27@gmail.com>
2023-04-09 21:50:08 +0800

    Initial Commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,22 @@
+package samples.demo;
+
+public class Location {
+	
+	private String location;
+	
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	public String getLocation() {
+		return this.location;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
